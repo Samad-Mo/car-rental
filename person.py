0bb59419e14fbd203bd11e1fc402590e8d250cc6
Samad-Mo <abdussamadmohit1@gmail.com>
2022-07-14 21:48:53 +0100

    i think the error was that you forgot to add self as the first parameter in the function deduct_money

--- person.py
+++ person.py
@@ -4,10 +4,8 @@ class Person:
         self.address = address
         self.total_money = 100
         
-    def deduct_money(amount):
+    def deduct_money(self, amount):
         self.total_money -= amount
 
     def __str__(self):
         return f'{self.name} @ {self.address}'
-    
-
