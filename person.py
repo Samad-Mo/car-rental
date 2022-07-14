238f9abcf20e063733f0223279c1ada806f1700a
Mohammed Yaseen <mohammedysn15@gmail.com>
2022-07-14 20:36:33 +0100

    Add money

--- person.py
+++ person.py
@@ -2,6 +2,10 @@ class Person:
     def __init__(self, name, address):
         self.name = name
         self.address = address
+        self.total_money = 100
+        
+    def deduct_money(amount):
+        self.total_money -= amount
 
     def __str__(self):
         return f'{self.name} @ {self.address}'
